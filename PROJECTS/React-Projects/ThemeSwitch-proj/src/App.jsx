eca47ee95f99f13469ca08c71d8c9c8e6f8f40a5
itsvroy007 <itsvroy007@duck.com>
2026-07-18 14:53:01 +0530

    new proj init

---
+++ PROJECTS/React-Projects/ThemeSwitch-proj/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return(
+    <>
+      <div>
+        <h2 className='p-4 bg-amber-400 text-center'>Chai aur Code</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
