d0c5a786b94ed65f1e0f377d316b136e29f594ec
itsvroy007 <itsvroy007@duck.com>
2026-08-09 09:52:26 +0530

    theory added

--- Language/C++/OPPS/OPPS_basics.cpp
+++ Language/C++/OPPS/OPPS_basics.cpp
@@ -11,6 +11,54 @@ using namespace std;
 1. public -> data for accessable to all
 2. private -> data hiding
 3. protected
+
+
+Object-oriented programming (OOP) is a programming paradigm that focuses on using objects, which are instances of classes, to organize and structure code. OOP is widely used in software development, and proficiency in OOP is often a requirement for many programming jobs. 
+
+1. What is OOP?
+
+Object-oriented programming is a programming paradigm that uses objects and instances of classes to organize and structure code. OOP emphasizes encapsulation, inheritance, and polymorphism to make code more modular, extensible, and reusable.
+
+2. What is a class?
+
+A class is a blueprint for creating objects in OOP. It defines the properties and behaviors that objects of the class will have. Classes provide a way to organize code and encapsulate data and functionality.
+
+3. What is Garbage Collection in the OOP System?
+
+Object-oriented programming (OOP) is a popular programming paradigm that revolves around the concepts of objects and classes. In an OOP system, managing memory and objects effectively is important to prevent errors or system crashes. One way to handle memory-related tasks in an OOP system is through garbage collection, which removes unwanted objects to free up memory.
+
+4. Can Java applications be executed without following OOP?
+
+Java is a popular programming language that is based on the OOP system. It cannot be run without OOPS implementations. Meanwhile, languages like C++ can be used with or without OOPs. Practicing OOPS coding questions in both Java and C++ is important to prepare for technical questions related to OOPS concepts.
+
+5. What are interfaces?
+
+Interfaces are used for declaring a method without specifically defining it. They do not contain instructions or behaviors to be executed and are not templates or blueprints for the program. In contrast, classes are user-defined blueprints containing instructions or methods that must be passed through the objects. Structures are user-defined sets of variables that contain various data types.
+
+6. What are pure virtual functions?
+
+Pure virtual functions are abstract functions that only get declared in the base classes. They do not contain definitions in the base classes and must be redefined inside the subclasses. Abstraction is a method of hiding details that are not required from important ones. Exceptions are special occurrences or events that can lead to execution failure or stopping.
+
+7. What is a superclass?
+
+Superclasses refer to the classes that allow other classes or any other class to inherit functions or behavior. Constructors are special methods that help initialize objects, while destructors are special methods that free up memory or resources occupied by objects. Destructors are used when objects need to be removed and destroyed and are automatically called during these scenarios.
+There are many more questions like the following:
+
+    Can you create an instance of an abstract class?
+
+    What are virtual functions?
+
+    What is the use of ‘finalize’?
+
+    Differentiate between an abstract class and an interface?
+
+    What is an exception?
+
+    What is exception handling?
+
+    What is the difference between an error and an exception?
+
+
 */
 
 
